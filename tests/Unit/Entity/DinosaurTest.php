e0515d16e366f96c525504fce96621e9784046f1
LDzoni <aleksicnikola46@gmail.com>
2023-01-04 14:18:31 +0100

    data providers

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -45,35 +45,44 @@ class DinosaurTest extends TestCase
 // Step 2: Run your test and watch it fail... since we haven't created that feature yet!
 // Step 3: Write as little code as possible to get our test to pass. And
 // Step 4: Now that it's passing, refactor your code if needed to make it more awesome
-    public function testDinosaurOver10MetersOrGreaterIsLarge(): void
+
+    /** @dataProvider sizeDescriptionProvider */
+    public function testDinoHasCorrectSizeDescriptionFromLength(int $length, string $expectedSize): void
     {
-        $dino = new Dinosaur(name: 'Big Eaty', length: 10 );
+        $dino = new Dinosaur(name: 'Big Eaty', length: $length );
 
         self::assertSame(
-            'Large',
-            $dino->getSizeDescription(),
-            'This is supposed to be a Large Dinosaurs'
-        ); //that is TDD (step 1 is done)
-    }
-
-    public function testDinosaurBetween5And9MetersIsMedium(): void
-    {
-        $dino = new Dinosaur(name: 'Big Eaty', length: 5 );
-
-        self::assertSame(
-            'Medium',
-            $dino->getSizeDescription(),
-            'This is supposed to be a Medium Dinosaurs'
+            $expectedSize,
+            $dino->getSizeDescription()
         );
     }
-    public function testDinosaurUnder5MetersIsSmall(): void
-    {
-        $dino = new Dinosaur(name: 'Big Eaty', length: 4 );
 
-        self::assertSame(
-            'Small',
-            $dino->getSizeDescription(),
-            'This is supposed to be a Small Dinosaurs'
-        );
+//    public function testDinosaurBetween5And9MetersIsMedium(): void
+//    {
+//        $dino = new Dinosaur(name: 'Big Eaty', length: 5 );
+//
+//        self::assertSame(
+//            'Medium',
+//            $dino->getSizeDescription(),
+//            'This is supposed to be a Medium Dinosaurs'
+//        );
+//    }
+//    public function testDinosaurUnder5MetersIsSmall(): void
+//    {
+//        $dino = new Dinosaur(name: 'Big Eaty', length: 4 );
+//
+//        self::assertSame(
+//            'Small',
+//            $dino->getSizeDescription(),
+//            'This is supposed to be a Small Dinosaurs'
+//        );
+//    }
+
+    // Data Providers
+    public function sizeDescriptionProvider(): \Generator
+    {
+        yield '10 Meter Large Din' => [10, 'Large'];
+        yield '5 Meter Medium Din' => [5, 'Medium'];
+        yield '4 Meter Small Din' => [4, 'Small'];
     }
 }
