9a3c6a479a5abe734e3c66c159f4b862f665cef5
LDzoni <aleksicnikola46@gmail.com>
2023-01-04 11:50:52 +0100

    covered all four steps to create TDD unit

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -39,4 +39,41 @@ class DinosaurTest extends TestCase
         self::assertSame(15, $dino->getLength());
         self::assertSame('Paddock A', $dino->getEnclosure());
     }
+
+//    TDD
+// Step 1: Write a test for the feature.
+// Step 2: Run your test and watch it fail... since we haven't created that feature yet!
+// Step 3: Write as little code as possible to get our test to pass. And
+// Step 4: Now that it's passing, refactor your code if needed to make it more awesome
+    public function testDinosaurOver10MetersOrGreaterIsLarge(): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: 10 );
+
+        self::assertSame(
+            'Large',
+            $dino->getSizeDescription(),
+            'This is supposed to be a Large Dinosaurs'
+        ); //that is TDD (step 1 is done)
+    }
+
+    public function testDinosaurBetween5And9MetersIsMedium(): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: 5 );
+
+        self::assertSame(
+            'Medium',
+            $dino->getSizeDescription(),
+            'This is supposed to be a Medium Dinosaurs'
+        );
+    }
+    public function testDinosaurUnder5MetersIsSmall(): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: 4 );
+
+        self::assertSame(
+            'Small',
+            $dino->getSizeDescription(),
+            'This is supposed to be a Small Dinosaurs'
+        );
+    }
 }
